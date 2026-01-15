d93d395ec07604040f588385d5f388a04e2aa0a4
Bharath_tn <steja11100@gmail.com>
2026-01-14 23:33:57 -0600

    day_1_40min

--- hello.java
+++ hello.java
@@ -1,1 +1,14 @@
 
+//jshell is introduced in java9
+//user(java code, .java file) -> Java Compiler(converts code to machine language, gives .class file ) ->JRE-inside jre there will be jvm( JVM(understands byte code) -> runs 1st file (public static void main))
+
+class hello
+{
+
+
+    public static void main(String args[])
+    {
+        System.out.println("hello world");
+
+    }
+}
