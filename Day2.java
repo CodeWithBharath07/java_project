fd6aee0e22bc7def26a7ac18452536b3a80b7a2b
Bharath_tn <steja11100@gmail.com>
2026-01-24 23:14:04 -0600

    day2

---
+++ Day2.java
@@ -0,0 +1,14 @@
+ class Day2
+ {
+
+ 
+ public static void main(String args[])
+    {
+        int num1 = 6;
+        int num2 = 4;
+        int res = num1+num2;
+
+        System.out.println(res);
+
+    }
+ }
