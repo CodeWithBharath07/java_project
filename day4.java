253a0f7dfbb66f0a08a5ac0aa676580008a232a0
Bharath_tn <steja11100@gmail.com>
2026-01-25 22:25:12 -0600

    conditional statements

--- day4.java
+++ day4.java
@@ -17,23 +17,52 @@
 //     }
 // }
 
-class day4
-{
-   public static void main(String[] args) {
+
+//**********logical operators******************* */
+// class day4
+// {
+//    public static void main(String[] args) {
        
-       int x =4;
-       int y=6;
-       int a=8;
-       int b=7;
+//        int x =4;
+//        int y=6;
+//        int a=8;
+//        int b=7;
 
-      boolean res = x<y || a>b;
-      boolean res1 = x>y && a>b;
-
-
-       System.out.println(res);
-
-       System.out.println(!res1);
+//       boolean res = x<y || a>b;
+//       boolean res1 = x>y && a>b;
 
 
-   } 
+//        System.out.println(res);
+
+//        System.out.println(!res1);
+
+
+//    } 
+// }
+
+
+//************Conditional statements***************** */
+class day4{
+
+    public static void main(String[] args) {
+        
+        int x = 8;
+        int y = 7;
+        int z = 9;
+
+        if(x>y && x>z)
+        {
+            System.out.println(x);
+
+        }
+        else if (y>z)
+         {
+         System.out.println(y);
+
+        }
+        else 
+        {
+            System.out.println(z);
+        }
+    }
 }
