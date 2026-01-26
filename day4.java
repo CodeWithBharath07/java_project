96a0cd4a9017ea97db84f882d9f2c07299b00403
Bharath_tn <steja11100@gmail.com>
2026-01-25 19:09:07 -0600

    operators

---
+++ day4.java
@@ -0,0 +1,39 @@
+//operators\\
+
+// pre increment : and post increment
+
+//   logical operatores: &&, ||, !
+
+// class day4
+// {
+//     public static void main(String[] args) {
+        
+//         int x=6;
+//         int y=5;
+
+//         boolean result = x<y;
+
+//         System.out.println(result);
+//     }
+// }
+
+class day4
+{
+   public static void main(String[] args) {
+       
+       int x =4;
+       int y=6;
+       int a=8;
+       int b=7;
+
+      boolean res = x<y || a>b;
+      boolean res1 = x>y && a>b;
+
+
+       System.out.println(res);
+
+       System.out.println(!res1);
+
+
+   } 
+}
