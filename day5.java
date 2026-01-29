6d19a5568b257ce101b63580b734d80e24e52764
Bharath_tn <steja11100@gmail.com>
2026-01-28 20:26:37 -0600

    switch statement

---
+++ day5.java
@@ -0,0 +1,22 @@
+//switch statement:
+
+class day5
+{
+    public static void main(String[] args) {
+
+        int n = 3;
+        if(n==1)
+        {
+        System.out.println("sunday");
+        }
+        else if(n==2)
+        {
+            System.out.println("Monday");
+        }
+        else if(n==3)
+        {
+            System.out.println("tuesday");
+        }
+
+    }
+}
